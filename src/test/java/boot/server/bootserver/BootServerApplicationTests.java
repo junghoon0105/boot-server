1e294fe983dac41756608a8e878b813ee2a34495
yjh <yjh@codemonster.kr>
2024-01-02 15:45:42 +0900

    git init

---
+++ src/test/java/boot/server/bootserver/BootServerApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.server.bootserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
